b948ca4dde1d7a680cedabe7b9d529124ec3e8eb
Eric Miller <eric@lakesareadesign.com>
2016-10-25 11:36:08 -0500

    Add Genesis Child ‘Gallery Pro’

---
+++ content/themes_genesis/gallery-pro/js/global.js
@@ -0,0 +1,13 @@
+jQuery( function ( $ ) {
+	// FitVids
+	$('.entry').fitVids();
+
+	// Sticky Header options
+	var options = {
+			offset: 500
+	};
+
+	// Initialize with options
+	var header = new Headhesive('.site-header', options);
+
+});
